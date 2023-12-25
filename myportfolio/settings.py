395d65f183983d1b1ba1550ab0a12f254aff11f5
NeelMoradiya47 <neelmoradiya56@gmail.com>
2023-12-25 14:46:33 +0530

    25-12-2023

--- myportfolio/settings.py
+++ myportfolio/settings.py
@@ -46,13 +46,13 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware', # Added manually
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware', # Added manually
 ]
 
 ROOT_URLCONF = 'myportfolio.urls'
--- myportfolio/urls.py
+++ myportfolio/urls.py
@@ -17,19 +17,11 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import include, path
 
-from django.conf import settings
-from django.conf.urls.static import static
-from django.views.static import serve
-from django.conf.urls import url
-
 urlpatterns = [
     path('', include('portfolioapp.urls')),
     path('about_me/', include('portfolioapp.urls')),
     path('projects/', include('portfolioapp.urls')),
     path('contact_me/', include('portfolioapp.urls')),
     path('insert_data/', include('portfolioapp.urls')),
-    url(r'^media/(?P<path>.*)$', serve,{'document_root': settings.MEDIA_ROOT}),
-    url(r'^static/(?P<path>.*)$', serve,{'document_root': settings.STATIC_ROOT}),
     path('admin/', admin.site.urls),
 ]
-urlpatterns = urlpatterns + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
